e88cb2a0da95186934443516827d6c1491b04f14
LivioLucas <liviool123@gmail.com>
2023-09-21 01:44:26 -0300

    Botão de senha e confirmar senha do cadastre-se
    Consegui consertar ambos, o ícone de confirmar senha precisava de uma função só pra ele

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 function mostrarSenha(){
   let inputPass = document.getElementById('password')
-  let btnShowPass = document.getElementById('btn-password','btn-repassword')
+  let btnShowPass = document.getElementById('btn-password','btn-password1')
+
+  if(inputPass.type === 'password'){
+      inputPass.setAttribute('type','text')
+      btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill')
+  } else {
+      inputPass.setAttribute('type','password')
+      btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill',)   
+  }
+}
+function mostrarSenha1(){
+  let inputPass = document.getElementById('repassword')
+  let btnShowPass = document.getElementById('btn-repassword')
 
   if(inputPass.type === 'password'){
       inputPass.setAttribute('type','text')
